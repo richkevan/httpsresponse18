ac986ce7bfdce1952f19dfe5d68ca1677033d83f
Wen Yuan <vinyuan77@outlook.com>
2024-06-07 15:03:27 -0700

    code updated

--- read_task.py
+++ read_task.py
@@ -1,5 +1,5 @@
 def read_task() :
-    with open('F:/Code Platton/Day 3/Team_Project_Phone_Terminal/httpsresponse18/tasks.txt', 'r') as taskFile :
+    with open('tasks.txt', 'r') as taskFile :
         task_contents = [];
         for line in taskFile:
             task_contents.append(line.strip());
