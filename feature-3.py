e9d7a537a9ffa5f89b3c572b2b6617c15ed19daf
ksbleek <kyle.s.bleeker@gmail.com>
2024-06-07 16:13:27 -0500

    added return to main menu to search function.

--- feature-3.py
+++ feature-3.py
@@ -7,4 +7,6 @@ def search():
     prior_searches.append(search_term)
     print(prior_searches)
     webbrowser.open(f'https://www.google.com/search?q={search_term}')
-
+    any_key = input('Hit any key to return to menu...')
+    if any_key:
+        open_menu()
