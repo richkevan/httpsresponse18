5f5232f53028738a7f29491263835d474d40ed42
ksbleek <kyle.s.bleeker@gmail.com>
2024-06-07 15:53:15 -0500

    changed internet() to search() and moved history function to be a part of the search function

--- feature-3.py
+++ feature-3.py
@@ -1,11 +1,10 @@
 import webbrowser
 
-prior_searches = []
+#prior_searches = []  
 
-def internet():
+def search():
     search_term = input('What do you want to search?')
     prior_searches.append(search_term)
+    print(prior_searches)
     webbrowser.open(f'https://www.google.com/search?q={search_term}')
 
-def history():
-    print(prior_searches)
